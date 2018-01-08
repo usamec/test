321f314a18d0578cb263e7596c120fe70a89b9b4
Vlado Boza <bozavlado@gmail.com>
2018-01-08 14:06:09 +0100

    initial

---
+++ task/model.py
@@ -0,0 +1,13 @@
+data = list(map(float, open("data/numbers.txt")))
+
+avg = sum(data) / len(data)
+
+fmodel = open("result/model.txt", "w")
+print(avg, file=fmodel)
+fmodel.close()
+
+error = sum(map(lambda x: (x-avg)**2, data)) / len(data)
+
+fmetrics = open("metrics.txt", "w")
+print(error, file=fmetrics)
+fmetrics.close()
