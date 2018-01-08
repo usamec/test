0a6b37d78ca1bf3546d122a4a4aa3908683e5120
Vlado Boza <bozavlado@gmail.com>
2018-01-08 16:14:42 +0100

    stuff

--- task/model.py
+++ task/model.py
@@ -1,4 +1,9 @@
+import time
+
+
 data = list(map(float, open("data/numbers.txt")))
+
+print("Data loaded")
 
 avg = sum(data) / len(data)
 
@@ -11,3 +16,5 @@ error = sum(map(lambda x: (x-avg)**2, data)) / len(data)
 fmetrics = open("metrics.txt", "w")
 print(error, file=fmetrics)
 fmetrics.close()
+
+print("Done")
